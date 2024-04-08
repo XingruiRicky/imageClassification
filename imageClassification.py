9970513c4f2ded95d7ea898a81a4cc47260fbdf4
Xingrui <56009624+XingruiRicky@users.noreply.github.com>
2024-04-07 22:19:56 -0600

    improve with dropout
    This reverts commit 53678c6e27da569dd54cec78732a79aeec64adc7.

--- imageClassification.py
+++ imageClassification.py
@@ -4,6 +4,7 @@ from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
 # 数据集的路径
 train_dir = 'dataset/train'
@@ -64,11 +65,27 @@ for layer in base_model.layers:
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # 训练模型
-model.fit(train_generator, epochs=10, validation_data=val_generator)
+history = model.fit(train_generator, epochs=10, validation_data=val_generator)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+epochs = range(1, len(acc) + 1)
+
+plt.figure(figsize=(12, 6))
+plt.plot(epochs, acc, 'b', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+model.save('model.h5')
 
 # 评估模型
 loss, accuracy = model.evaluate(test_generator)
 print(f"Test accuracy: {accuracy*100}%")
+
 
 
 # CNN模型的层结构：
@@ -80,10 +97,12 @@ print(f"Test accuracy: {accuracy*100}%")
 # 隐藏层的概念：
 # 严格来说，池化层可以被视为隐藏层的一种，因为它不直接与最终的输出相连，而是在内部处理特征表示。
 # 通常，术语“隐藏层”用于指代那些不是输入层或输出层的层，因为它们“隐藏”在网络的内部，执行特征提取和变换。
+
 # 前向传播和反向传播：
 # 在前向传播过程中，数据通过网络的每一层进行传递，并产生最终输出。
 # 如果输出与预期不符（例如，分类错误），损失函数将计算一个损失值，表示预测的错误程度。
 # 然后，在反向传播过程中，根据损失函数相对于网络参数的梯度，调整网络权重，以减少未来预测的损失。
+
 # 损失函数和优化器：
 # 损失函数定义了模型预测和实际标签之间的差距。常见的损失函数包括交叉熵损失（用于分类任务）和均方误差损失（用于回归任务）。
 # 优化器决定了如何根据损失函数计算的梯度来更新网络的权重。常见的优化器包括梯度下降、Adam 和 RMSprop。
